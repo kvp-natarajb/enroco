e98657dfb8e3fae7ce21d80e5ccc67872349323e
Nataraja B <nataraja.b@kvpcorp.com>
2018-06-24 01:03:22 +0530

    Applicatiion yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'https://enrocoapp.herokuapp.com' }
-  config.action_mailer.delivery_method = :ses
+  config.action_mailer.default_url_options = { host: ENV['HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -92,9 +91,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'gmail.com',
-    user_name:            'kuiqlydemo@gmail.com',
-    password:             'KuiqRoutes@123',
+    domain:               ENV['HOST'],
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
