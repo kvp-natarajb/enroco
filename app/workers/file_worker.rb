fa0982e111d58c64bed29090109c20a0d6459d5b
Nataraja B <nataraja.b@kvpcorp.com>
2018-06-23 14:02:29 +0530

    applicationdds

--- app/workers/file_worker.rb
+++ app/workers/file_worker.rb
@@ -1,6 +1,6 @@
 require 'csv'
 require 'sidekiq'
-require 'open-uri'
+require 'uri'
 # require 'sidekiq/testing/inline'
 include Sidekiq::Worker
 include SidekiqStatus::Worker
@@ -11,7 +11,7 @@ class FileWorker
   	@user  = User.find_by_id(user_id)
   	res    = true
 
-    CSV.foreach(file_path, headers: true) do |row|
+    CSV.foreach(URI.parse(file_path), headers: true) do |row|
     	hash = row.to_h
       @inpu_array = Array.new(hash.length){Array.new} if res
 
