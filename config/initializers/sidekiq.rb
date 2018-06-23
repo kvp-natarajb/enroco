c2d5f15bdac2fa1c3f075f0dec15fa4f06f4e415
Nataraja B <nataraja.b@kvpcorp.com>
2018-06-23 11:25:16 +0530

    sidekiq rb file

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,7 @@
+Sidekiq.configure_client do |config|
+  config.redis = { :size => 1 }
+end
+# so one sidekiq can have 7 connections
+Sidekiq.configure_server do |config|
+  config.redis = { :size => 10 }
+end
