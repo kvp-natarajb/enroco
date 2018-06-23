9a9c88fd9ab4b96f663f902e7bb376486d828548
Nataraja B <nataraja.b@kvpcorp.com>
2018-06-23 13:33:22 +0530

    application shkds

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,23 @@
 require 'sidekiq'
 
 Sidekiq.configure_client do |config|
-  config.redis = { :size => ENV['CLIENT_SIZE'] }
+  config.redis = { url: ENV['REDISTOGO_URL'], size: ENV['CLIENT_SIZE'] }
 end
 
 Sidekiq.configure_server do |config|
-  # The config.redis is calculated by the 
-  # concurrency value so you do not need to 
-  # specify this. For this demo I do 
-  # show it to understand the numbers
-  config.redis = { :size => ENV['SERVER_SIZE'] }
-end
+  config.redis = { url: ENV['REDISTOGO_URL'], size: ENV['SERVER_SIZE'] }
+
+  Rails.application.config.after_initialize do
+    Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+    ActiveRecord::Base.connection_pool.disconnect!
+
+    ActiveSupport.on_load(:active_record) do
+      config = Rails.application.config.database_configuration[Rails.env]
+      config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
+      # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || `
+      config['pool'] = ENV["DB_POOL"].to_i - 2 
+      ActiveRecord::Base.establish_connection(config)
+
+      Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+    end
+  end
